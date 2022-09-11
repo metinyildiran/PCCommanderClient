09cc4f0cb0ef560a1c4fc1a044bf2102f59428d5
Metin Yıldıran <metin.1464@gmail.com>
2022-09-11 12:05:47 +0300

    Deleted old connection method

--- app/src/main/java/com/example/pccommanderclient/dependencyinjection/Connection.kt
+++ app/src/main/java/com/example/pccommanderclient/dependencyinjection/Connection.kt
@@ -1,39 +1,17 @@
 package com.example.pccommanderclient.dependencyinjection
 
-import android.os.StrictMode
 import com.example.pccommanderclient.service.CommandAPI
-import com.example.pccommanderclient.util.SentResult
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.io.IOException
-import java.net.Socket
-import java.net.UnknownHostException
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class Connection {
-
-    @Singleton
-    @Provides
-    fun setupSocket(): SentResult<Socket> {
-        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy)
-
-        return try {
-            val socket = Socket("192.168.1.35", 1755)
-
-            SentResult.Success(socket)
-        } catch (uhe: UnknownHostException) {
-            SentResult.Error(uhe.message!!)
-        } catch (io: IOException) {
-            SentResult.Error(io.message!!)
-        }
-    }
 
     @Singleton
     @Provides
