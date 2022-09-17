cb1d5d30c624f3320d0fef6db8f7159799e0bed8
Metin Yıldıran <metin.1464@gmail.com>
2022-09-17 15:10:37 +0300

    Added ability to send/receive data from other apps and changed logo

--- app/src/main/java/com/example/pccommanderclient/MainActivity.kt
+++ app/src/main/java/com/example/pccommanderclient/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.pccommanderclient
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.pccommanderclient.model.Command
 import com.example.pccommanderclient.ui.theme.PCCommanderClientTheme
-import com.example.pccommanderclient.view.CommandScreen
+import com.example.pccommanderclient.viewmodel.CommandViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,14 +29,81 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PCCommanderClientTheme {
-                val navController = rememberNavController()
+                val viewModel: CommandViewModel = hiltViewModel()
 
-                NavHost(navController = navController, startDestination = "command_screen") {
-                    composable("command_screen") {
-                        CommandScreen()
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Button(modifier = Modifier.width(200.dp), onClick = {
+                        viewModel.sendCommand(Command("cmd /c start chrome https://www.youtube.com/feed/subscriptions"))
+
+                    }) {
+                        Text(text = "Start Youtube")
+                    }
+
+                    Button(modifier = Modifier.width(200.dp), onClick = {
+                        viewModel.sendCommand(Command("rundll32.exe powrprof.dll SetSuspendState Sleep"))
+                    }) {
+                        Text(text = "Hibernate")
+                    }
+
+                    Button(onClick = { shareData() }) {
+                        Text(text = "Send Text")
+                    }
+                }
+
+                when {
+                    intent?.action == Intent.ACTION_SEND -> {
+                        if ("text/plain" == intent.type) {
+                            handleSendText(viewModel, intent)
+                        } else if (intent.type?.startsWith("image/") == true) {
+                            handleSendImage(intent)
+                        }
+                    }
+                    intent?.action == Intent.ACTION_SEND_MULTIPLE
+                            && intent.type?.startsWith("image/") == true -> {
+                        handleSendMultipleImages(intent)
+                    }
+                    else -> {
+                        // Handle other intents, such as being started from the home screen
                     }
                 }
             }
         }
+    }
+
+    private fun handleSendText(viewModel: CommandViewModel, intent: Intent) {
+        intent.getStringExtra(Intent.EXTRA_TEXT)?.let { incomingText ->
+            viewModel.sendCommand(Command("cmd /c start chrome $incomingText"))
+            finishAffinity()
+        }
+    }
+
+    private fun handleSendImage(intent: Intent) {
+        (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let { incomingImage ->
+
+        }
+    }
+
+    private fun handleSendMultipleImages(intent: Intent) {
+        intent.getParcelableArrayListExtra<Parcelable>(Intent.EXTRA_STREAM)
+            ?.let { incomingMultipleImages ->
+
+            }
+    }
+
+    private fun shareData() {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
     }
 }
