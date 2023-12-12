94a7d87a7915e05f16fb985f126c46697a02a134
Metin Yıldıran <metin.1464@gmail.com>
2023-12-12 22:26:41 +0300

    Added dynamic color

--- app/src/main/java/com/example/pccommanderclient/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pccommanderclient/ui/theme/Theme.kt
@@ -1,10 +1,19 @@
 package com.example.pccommanderclient.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 
 private val LightColors = lightColorScheme(
@@ -73,17 +82,30 @@ fun AppTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    var colors = if (!useDarkTheme) {
-        LightColors
-    } else {
-        DarkColors
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+
+    val colorScheme = when {
+        dynamicColor && useDarkTheme -> {
+            dynamicDarkColorScheme(LocalContext.current)
+        }
+        dynamicColor && !useDarkTheme -> {
+            dynamicLightColorScheme(LocalContext.current)
+        }
+        useDarkTheme -> DarkColors
+        else -> LightColors
     }
 
-    if (android.os.Build.VERSION.SDK_INT <= 29)
-        colors = DarkColors
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !useDarkTheme
+        }
+    }
 
     MaterialTheme(
-        colorScheme = colors,
+        colorScheme = colorScheme,
         content = content
     )
 }
